3e7bfbfd1a262707b659c5308b5bb7da2f57780d
Dennis Duffy <dennis.duffy@state.mn.us>
2026-03-05 14:52:13 -0600

    updated scripts for March 2026

--- scripts/IRP Cleanup.R
+++ scripts/IRP Cleanup.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 library(tidyxl)
 library(janitor)
+library(here)
 
-irp <- xlsx_cells("IRP DATA 10-1-25 SR saved copy.xlsx")
+irp <- xlsx_cells(here("raw_data/IRP_Cleanup_3-2-2026 modified.xlsx")) # change with new IRP data
 
 # Clean up nameplate capacity
 
@@ -10,20 +11,20 @@ capacity_cleaner <- function(sheet_name, utility){
   
   fuel_types <- irp |> 
     filter(sheet == sheet_name, 
-           row %in% c(1:17) & col == 1) |> 
+           row %in% c(1:17) & col == 1) |> # check these row numbers against the latest version of the spreadsheet
     select(character) |> 
     row_to_names(row_number = 1) |> 
     rename(fuel_type = 1)
   
   nameplate_values <- irp |> 
     filter(sheet == sheet_name, 
-           row %in% c(2:17) & col %in% c(2:15)) |> 
+           row %in% c(2:17) & col %in% c(2:15)) |> # check these row numbers against the latest version of the spreadsheet
     select(row, col, numeric) |> 
     pivot_wider(names_from = col, values_from = numeric)
   
   nameplate_colnames <- irp |> 
     filter(sheet == sheet_name, 
-           row == 1, col %in% c(1:15)) |> 
+           row == 1, col %in% c(1:15)) |> # check these row numbers against the latest version of the spreadsheet
     mutate(values_use = case_when(
       data_type == "character" ~ character, 
       data_type == "numeric" ~ content
@@ -40,7 +41,7 @@ capacity_cleaner <- function(sheet_name, utility){
                               "Demand:Energy Efficiency", "Other:Other", "Renewable:Biomass", "Renewable:Landfill", "Demand:Demand Response"))) |> 
     mutate(fuel_type = case_when(
       fuel_type == "Coal:Conventional" ~ "Coal",
-      fuel_type %in% c("Gas/Oil:Combined Cycle", "Gas/Oil:Combustion Turbine") ~ "Natural Gas",
+      fuel_type %in% c("Gas/Oil:Combined Cycle", "Gas/Oil:Combustion Turbine", "Gas/Oil:Steam Turbine") ~ "Natural Gas",
       fuel_type == "Hydro:Hydroelectric" ~ "Hydroelectric",
       fuel_type == "Nuclear:Nuclear" ~ "Nuclear",
       fuel_type == "Renewable:Solar PV" ~ "Solar",
@@ -77,7 +78,7 @@ nameplate_projections <- capacity_cleaner(sheet_name = "Xcel", utility = "Xcel E
   bind_rows(capacity_cleaner(sheet_name = "MP", utility = "Minnesota Power")) |> 
   bind_rows(capacity_cleaner(sheet_name = "OTP", utility = "Otter Tail Power"))
 
-#write_csv(nameplate_projections, "C:/Users/dduffy/OneDrive - State of Minnesota - MN365/Quad 2.0 Data - Documents/data/irp_nameplate_projections.csv", na = "")
+write_csv(nameplate_projections, "I:/Enrgy_div/SEO/CleanEnegyTechUnit/CET Projects/Data Repository/irp-cleanup/processed_data/irp_nameplate_projections.csv", na = "")
 
 # Clean up Energy Mix
 
@@ -85,21 +86,18 @@ mix_cleaner <- function(sheet_name, utility){
   
   fuel_types <- irp |> 
     filter(sheet == sheet_name, 
-           row %in% c(19:35) & col == 1) |> 
-    select(character) |> 
-    row_to_names(row_number = 1) |> 
-    rename(fuel_type = 1)
-  
+           row %in% 21:36, col == 1) |> 
+    select(fuel_type = character)
   
   production_values <- irp |> 
     filter(sheet == sheet_name, 
-           row %in% c(20:35) & col %in% c(2:15)) |> 
+           row %in% c(21:36) & col %in% c(2:15)) |> # check these row numbers against the latest version of the spreadsheet
     select(row, col, numeric) |> 
     pivot_wider(names_from = col, values_from = numeric)
   
   production_colnames <- irp |> 
     filter(sheet == sheet_name, 
-           row == 19, col %in% c(1:15)) |> 
+           row == 20, col %in% c(1:15)) |> # check these row numbers against the latest version of the spreadsheet
     mutate(values_use = case_when(
       data_type == "character" ~ character, 
       data_type == "numeric" ~ content
@@ -116,7 +114,7 @@ mix_cleaner <- function(sheet_name, utility){
                               "Demand:Energy Efficiency", "Other:Other", "Renewable:Landfill", "Demand:Demand Response"))) |> 
     mutate(fuel_type = case_when(
       fuel_type == "Coal:Conventional" ~ "Coal",
-      fuel_type %in% c("Gas/Oil:Combined Cycle", "Gas/Oil:Combustion Turbine") ~ "Natural Gas",
+      fuel_type %in% c("Gas/Oil:Combined Cycle", "Gas/Oil:Combustion Turbine", "Gas/Oil:Steam Turbine") ~ "Natural Gas",
       fuel_type == "Hydro:Hydroelectric" ~ "Hydroelectric",
       fuel_type == "Nuclear:Nuclear" ~ "Nuclear",
       fuel_type == "Renewable:Solar PV" ~ "Solar",
@@ -152,8 +150,7 @@ energy_mix <- mix_cleaner(sheet_name = "Xcel", utility = "Xcel Energy") |>
   bind_rows(mix_cleaner(sheet_name = "OTP", utility = "Otter Tail Power"))
 
 
-#write_csv(energy_mix, "C:/Users/dduffy/OneDrive - State of Minnesota - MN365/Quad 2.0 Data - Documents/data/irp_energy_mix_projections.csv", na = "")
-write_csv(energy_mix, "I:/Enrgy_div/SEO/CleanEnegyTechUnit/CET Projects/Data Repository/Utility IRPs/irp_energy_mix_projections.csv", na = "")
+write_csv(energy_mix, "I:/Enrgy_div/SEO/CleanEnegyTechUnit/CET Projects/Data Repository/irp-cleanup/processed_data/irp_energy_mix_projections.csv", na = "")
 
 
 
